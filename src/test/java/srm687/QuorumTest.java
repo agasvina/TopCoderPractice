54e9af386ee7fde7dcd37caea93409a15abbc8a8
asilvina <ASilvina@ie.ibm.com>
2016-05-03 22:00:16 +0100

    Removed License

--- src/test/java/srm687/QuorumTest.java
+++ src/test/java/srm687/QuorumTest.java
@@ -1,14 +1,3 @@
-/*
- * IBM Confidential
- *
- * OCO Source Materials
- *
- * © Copyright IBM Corp. 2015, 2016
- *
- * The source code for this program is not published or otherwise
- * divested of its trade secrets, irrespective of what has been
- * deposited with the U.S. Copyright Office.
- */
 package srm687;
 
 import static org.junit.Assert.assertEquals;
